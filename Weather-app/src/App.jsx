e23a0aed2548fb117b2414b8bcbbd6a59d92dc6d
ellaacquaboah <emmanuellaakwaboah@gmail.com>
2024-10-05 18:46:09 +0000

    reates the project by using the [npm create vite@latest]
    Installing all react packages needed [npm install]
    
    Installing tailwind css
    1) npm install -D tailwindcss postcss autoprefixer
    2) npx tailwindcss init -p
    
    Setting up the tailwind.config.js file and the css files

---
+++ Weather-app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className='text-xl font-bold text-center'>
+      Hello world!! welcome today's weather app.
+    </div>
+  )
+}
+
+export default App
